fa0454145d48143726045dc7315c854655f7051e
dark-flames <dark_flames@outlook.com>
2021-10-25 16:13:59 +0900

    VIEW: use macro to generate numeric view

--- core/src/view/numeric.rs
+++ core/src/view/numeric.rs
@@ -3,26 +3,37 @@ use crate::entity::FieldView;
 use crate::expr::View;
 use crate::query::computation::Computation;
 use crate::query::optimizer::QueryOptimizer;
-use crate::resolver::field_resolve_cells::numeric::ShortDataConverter;
+use crate::resolver::field_resolve_cells::numeric::*;
 
-pub struct ShortFieldView {
-    _column_name: String,
-    converter: ShortDataConverter
-}
+macro_rules! implement_view_of {
+    ($ty: ty, $name: ident, $converter: ty) => {
+        pub struct $name {
+            converter: $converter
+        }
 
-impl View for ShortFieldView {
-    type Output = i16;
+        impl View for $name {
+            type Output = $ty;
 
-    fn computation<'f>(&self) -> Computation<'f, Self::Output> {
-        Computation::create(self.converter.field_value_converter())
+            fn computation<'f>(&self) -> Computation<'f, Self::Output> {
+                Computation::create(self.converter.field_value_converter())
+            }
+
+            fn optimizer(&self) -> Box<dyn QueryOptimizer> {
+                todo!("add column in select list")
+            }
+        }
+
+        impl FieldView for $name {
+            type Type = $ty;
+        }
     }
-
-    fn optimizer(&self) -> Box<dyn QueryOptimizer> {
-        todo!("add column in select list")
-    }
 }
 
-impl FieldView for ShortFieldView {
-    type Type = i16;
-}
+implement_view_of!(i16, ShortFieldView, ShortDataConverter);
+implement_view_of!(u16, UnsignedShortFieldView, UnsignedShortDataConverter);
+implement_view_of!(i32, IntFieldView, IntDataConverter);
+implement_view_of!(u32, UnsignedIntFieldView, UnsignedIntDataConverter);
+implement_view_of!(i64, LongFieldView, LongDataConverter);
+implement_view_of!(u64, UnsignedLongFieldView, UnsignedLongDataConverter);
+
 
