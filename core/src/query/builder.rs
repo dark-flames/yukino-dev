e9236d53ff97cb559e1b0ea27fa82fd32158e9f4
dark-flames <dark_flames@outlook.com>
2021-11-11 22:21:10 +0900

    QUERY：QueryResultFilter

--- core/src/query/builder.rs
+++ core/src/query/builder.rs
@@ -1,1 +1,22 @@
+use std::marker::PhantomData;
+use crate::interface::{Entity, EntityView};
+use crate::query::{Alias, Expr};
+use crate::view::{ExprViewBox, Value};
 
+pub struct QueryResultFilter<
+    E: Entity
+> {
+    filter: Vec<Expr>,
+    root_alias: Alias,
+    _entity: PhantomData<E>
+}
+impl<
+    E: Entity
+> QueryResultFilter<E> {
+    pub fn filter<F>(&mut self, f: F)
+        where F: Fn(E::View) -> ExprViewBox<bool, <bool as Value>::L> {
+        let view = f(E::View::pure(&self.root_alias));
+        // join
+        self.filter.extend(view.collect_expr());
+    }
+}
