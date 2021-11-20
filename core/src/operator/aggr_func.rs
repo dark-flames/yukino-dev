c77b7b2300b8be0bb48de26182dc496d6fd91319
dark-flames <dark_flames@outlook.com>
2021-11-21 03:10:32 +0900

    OPERATOR: impl aggregate expr

--- core/src/operator/aggr_func.rs
+++ core/src/operator/aggr_func.rs
@@ -3,7 +3,7 @@ use std::marker::PhantomData;
 use generic_array::{arr, GenericArray};
 use generic_array::typenum::U1;
 
-use query_builder::{DatabaseValue, Expr, ExprMutVisitor, ExprNode, ExprVisitor, FunctionCall};
+use query_builder::{DatabaseValue, Expr, ExprMutVisitor, ExprNode, ExprVisitor, Function, FunctionCall};
 
 use crate::err::{RuntimeResult, YukinoError};
 use crate::view::{ExprView, ExprViewBox, Value, ValueCountOf, View, ViewBox};
@@ -60,14 +60,67 @@ impl<T: Value<L=U1>> AggregateView<T> {
     }
 }
 
-pub trait ExprAverage: Value {
-    type Result: Value<L=U1>;
+macro_rules! impl_aggregate {
+    (
+        $trait_name: ident,
+        $trait_method: ident,
+        $method_name: ident,
+        $macro_name: ident,
+        $variant: ident,
+        [$($types: ty),*]
+    ) => {
+        pub trait $trait_name: Value {
+            type Result: Value<L=U1>;
 
-    fn expr_average(expr: ExprViewBox<Self>) -> AggregateView<Self::Result>;
+            fn $trait_method(expr: ExprViewBox<Self>) -> AggregateView<Self::Result>;
+        }
+
+        pub fn $method_name<V: Value + $trait_name>(
+            v: ExprViewBox<V>,
+        ) -> AggregateView<<V as $trait_name>::Result> {
+            V::$trait_method(v)
+        }
+
+        macro_rules! $macro_name {
+            ($ty: ty) => {
+                impl $trait_name for $ty {
+                    type Result = $ty;
+
+                    fn $trait_method(expr: ExprViewBox<Self>) -> AggregateView<Self::Result> {
+                        AggregateView::from_function_call(FunctionCall {
+                            func: Function::$variant,
+                            params: vec![expr.collect_expr().into_iter().next().unwrap()]
+                        })
+                    }
+                }
+            };
+        }
+
+        $($macro_name!($types);)*
+    }
 }
 
-pub fn average<V: Value + ExprAverage>(
-    v: ExprViewBox<V>,
-) -> AggregateView<<V as ExprAverage>::Result> {
-    V::expr_average(v)
+impl_aggregate!(ExprsAverage, exprs_average, average, impl_average, Average, [
+    u16, i16, u32, i32, u64, i64, f32, f64]);
+impl_aggregate!(ExprsBitAnd, exprs_bit_and, bit_and, impl_bit_and, BitAnd, [
+    u16, i16, u32, i32, u64, i64]);
+impl_aggregate!(ExprsBitOr, exprs_bit_or, bit_or, impl_bit_or, BitOr, [
+    u16, i16, u32, i32, u64, i64]);
+impl_aggregate!(ExprsBitXor, exprs_bit_xor, bit_xor, impl_bit_xor, BitXor, [
+    u16, i16, u32, i32, u64, i64]);
+impl_aggregate!(ExprsConcat, exprs_concat, concat, impl_concat, Concat, [String]);
+impl_aggregate!(ExprsMin, exprs_min, min, impl_min, Min, [
+    u16, i16, u32, i32, u64, i64, f32, f64, char, String]);
+impl_aggregate!(ExprsMax, exprs_max, max, impl_max, Max, [
+    u16, i16, u32, i32, u64, i64, f32, f64, char, String]);
+
+impl ExprsConcat for char {
+    type Result = String;
+
+    fn exprs_concat(expr: ExprViewBox<Self>) -> AggregateView<Self::Result> {
+        AggregateView::from_function_call(FunctionCall {
+            func: Function::Concat,
+            params: vec![expr.collect_expr().into_iter().next().unwrap()],
+        })
+    }
 }
