7b8330def514eacca299486c7a9d412bd94be12d
dark-flames <dark_flames@outlook.com>
2021-12-21 08:21:14 +0900

    TEST: add a demo

--- tests/src/meeting.rs
+++ tests/src/meeting.rs
@@ -9,6 +9,7 @@ pub struct Person {
     pub id: u32,
     pub name: String,
     pub age: u32,
+    pub level: u16,
 }
 
 #[derive(Entity, Clone, Debug)]
@@ -22,7 +23,7 @@ pub struct Meeting {
     pub end_time: u64,
 }
 
-pub async fn adult_hosted_meeting_length<'c, 'e, E: 'e + Executor<'c, Database = MySql>>(
+pub async fn adult_hosted_meeting_length<'c, E: Executor<'c, Database = MySql>>(
     executor: E,
 ) -> Vec<u64> {
     let adult = Person::all().filter(|p| bt!(p.age, 18));
@@ -33,3 +34,20 @@ pub async fn adult_hosted_meeting_length<'c, 'e, E: 'e + Executor<'c, Database =
         .await
         .unwrap()
 }
+
+pub async fn average_meeting_count_by_level<'c, E: Executor<'c, Database = MySql>>(
+    executor: E,
+) -> Vec<(u16, u64)> {
+    Person::all()
+        .group_by(|p| p.level)
+        .fold_group(|p| {
+            p.map(|p| {
+                Meeting::belonging_to_view(&p)
+                    .fold(|m| m.id.count())
+                    .as_expr()
+            }).average()
+        }).exec(executor)
+        .await
+        .unwrap()
+}
+
