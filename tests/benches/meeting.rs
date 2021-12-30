07dec8e71c36f6b89a45c1937c1fd6d8d0db1a99
dark-flames <dark_flames@outlook.com>
2021-12-30 13:43:02 +0900

    CHORE：add profile

---
+++ tests/benches/meeting.rs
@@ -0,0 +1,188 @@
+use std::env;
+
+use criterion::{Criterion, criterion_main};
+use criterion::criterion_group;
+use sqlx::{Executor, MySql, MySqlPool};
+use sqlx::mysql::MySqlPoolOptions;
+use sqlx::types::Decimal;
+use tokio::runtime::Runtime;
+
+use yukino::prelude::*;
+
+// This is a struct that tells Criterion.rs to use the "futures" crate's current-thread executor
+
+#[derive(Entity, Clone, Debug)]
+pub struct Person {
+    #[id]
+    pub id: u32,
+    pub name: String,
+    pub age: u32,
+    pub level: u16,
+}
+
+#[derive(Entity, Clone, Debug)]
+pub struct Meeting {
+    #[id]
+    pub id: u32,
+    pub title: String,
+    #[belongs_to(Person)]
+    pub host_id: u32,
+    pub start_time: u64,
+    pub end_time: u64,
+}
+
+pub async fn adult_hosted_meeting_length<'c, E: Executor<'c, Database = MySql>>(
+    executor: E,
+) -> Vec<u64> {
+    let adult = Person::all().filter(|p| bt!(p.age, 18));
+
+    Meeting::belonging_to_query(adult)
+        .sort(|m| m.id.asc())
+        .map(|m| m.end_time - m.start_time)
+        .exec(executor)
+        .await
+        .unwrap()
+}
+
+pub async fn meeting_count_by_level<'c, E: Executor<'c, Database = MySql>>(
+    executor: E,
+) -> Vec<(u16, Option<Decimal>)> {
+    Person::all()
+        .group_by(|p| p.level)
+        .fold_group(|p| {
+            p.map(|p| {
+                Meeting::belonging_to_view(&p)
+                    .fold(|m| m.id.count())
+                    .as_expr()
+            })
+                .sum()
+        })
+        .exec(executor)
+        .await
+        .unwrap()
+}
+
+pub async fn hosted_meeting_titles<'c, E: Executor<'c, Database = MySql>>(
+    executor: E,
+) -> Vec<(u32, Option<String>)> {
+    Person::all()
+        .map(|p| {
+            (
+                p.id.clone(),
+                Meeting::belonging_to_view(&p)
+                    .fold(|m| m.sort(|m| m.id.asc()).map(|m| m.title).join(Some(", ")))
+                    .as_expr(),
+            )
+        })
+        .exec(executor)
+        .await
+        .unwrap()
+}
+
+pub async fn prepare_data(pool: &MySqlPool) {
+    let person_list = vec![
+        Person {
+            id: 1,
+            name: "Alice".to_string(),
+            age: 15,
+            level: 1,
+        },
+        Person {
+            id: 2,
+            name: "Bob".to_string(),
+            age: 19,
+            level: 1,
+        },
+        Person {
+            id: 3,
+            name: "Carol".to_string(),
+            age: 20,
+            level: 2,
+        },
+        Person {
+            id: 4,
+            name: "David".to_string(),
+            age: 17,
+            level: 2,
+        },
+    ];
+    let meeting_list = vec![
+        Meeting {
+            id: 1,
+            title: "Meeting 1".to_string(),
+            host_id: 1,
+            start_time: 1,
+            end_time: 10,
+        },
+        Meeting {
+            id: 2,
+            title: "Meeting 2".to_string(),
+            host_id: 1,
+            start_time: 2,
+            end_time: 11,
+        },
+        Meeting {
+            id: 3,
+            title: "Meeting 3".to_string(),
+            host_id: 2,
+            start_time: 3,
+            end_time: 12,
+        },
+        Meeting {
+            id: 4,
+            title: "Meeting 4".to_string(),
+            host_id: 3,
+            start_time: 2,
+            end_time: 11,
+        },
+        Meeting {
+            id: 5,
+            title: "Meeting 5".to_string(),
+            host_id: 3,
+            start_time: 3,
+            end_time: 12,
+        },
+    ];
+
+    for person in person_list {
+        person.insert().exec(pool).await.unwrap();
+    }
+
+    for meeting in meeting_list {
+        meeting.insert().exec(pool).await.unwrap();
+    }
+}
+
+pub async fn delete_all(pool: &MySqlPool) {
+    Person::all().delete().exec(pool).await.unwrap();
+    Meeting::all().delete().exec(pool).await.unwrap();
+}
+
+pub async fn bench_group(pool: &MySqlPool) {
+    delete_all(pool).await;
+    prepare_data(pool).await;
+    adult_hosted_meeting_length(pool).await;
+    meeting_count_by_level(pool).await;
+    hosted_meeting_titles(pool).await;
+}
+
+pub fn run_bench_group(c: &mut Criterion) {
+    let url = env::var("DB").unwrap();
+    let runtime = Runtime::new().unwrap();
+
+    let pool = runtime.block_on(MySqlPoolOptions::new()
+        .max_connections(10)
+        .connect(&url)).unwrap();
+
+
+    c.bench_function("bench_meeting", |b| {
+        // Insert a call to `to_async` to convert the bencher to async mode.
+        // The timing loops are the same as with the normal bencher.
+        b.to_async(&runtime).iter(|| bench_group(&pool));
+    });
+}
+
+criterion_group!(benches, run_bench_group);
+criterion_main!(benches);
+
+
